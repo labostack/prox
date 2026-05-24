bd716bb7e59e14d9a7765312ba16017498e7405e
dortanes <hey@ayoo.pro>
2026-05-24 19:53:15 +0500

    feat(server): implement Linux socket optimizations with TCP_DEFER_ACCEPT

--- internal/server/listener_linux.go
+++ internal/server/listener_linux.go
@@ -1,4 +1,4 @@
-//go:build (unix || darwin || freebsd || openbsd || netbsd) && !linux
+//go:build linux
 
 package server
 
@@ -10,13 +10,18 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-// reusePortListen creates a net.Listener with the SO_REUSEPORT socket option enabled.
+// reusePortListen creates a net.Listener with SO_REUSEPORT and TCP_DEFER_ACCEPT enabled.
 func reusePortListen(network, addr string) (net.Listener, error) {
 	lc := net.ListenConfig{
 		Control: func(network, address string, c syscall.RawConn) error {
 			var opterr error
 			err := c.Control(func(fd uintptr) {
 				opterr = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+				if opterr != nil {
+					return
+				}
+				// 15 seconds wait timeout for incoming data before accepting anyway
+				opterr = unix.SetsockoptInt(int(fd), unix.IPPROTO_TCP, unix.TCP_DEFER_ACCEPT, 15)
 			})
 			if err != nil {
 				return err
